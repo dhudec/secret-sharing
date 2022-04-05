a1b2af80894bda9e5a61f3c133798161dc2efa18
Drew Hudec <drew@basistheory.com>
2022-04-05 16:46:02 -0400

    Adding localePath to i18n config

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,3 +1,5 @@
+const path = require('path');
+
 module.exports = {
   i18n: {
     defaultLocale: 'en',
@@ -6,5 +8,6 @@ module.exports = {
       useSuspense: false,
     },
     reloadOnPrerender: process.env.NODE_ENV !== 'production',
+    localePath: path.resolve('./public/locales'),
   },
 };
